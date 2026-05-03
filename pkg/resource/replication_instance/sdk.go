5d9e35cad171b77f11b40cd8d0a7edc840daa57f
Michael Göhler <github@nixhub.de>
2026-05-03 18:12:52 +0200

    No syncTags on delete

--- pkg/resource/replication_instance/sdk.go
+++ pkg/resource/replication_instance/sdk.go
@@ -706,9 +706,11 @@ func (rm *resourceManager) sdkUpdate(
 	ko := desired.ko.DeepCopy()
 
 	// sdk_update_pre_set_output hook
-	if delta.DifferentAt("Spec.Tags") {
-		if err = rm.syncTags(ctx, desired, latest); err != nil {
-			return nil, err
+	if latest.ko.ObjectMeta.GetDeletionTimestamp() == nil {
+		if delta.DifferentAt("Spec.Tags") {
+			if err = rm.syncTags(ctx, desired, latest); err != nil {
+				return nil, err
+			}
 		}
 	}
 
--- templates/hooks/replication_instance/sdk_update_pre_set_output.go.tpl
+++ templates/hooks/replication_instance/sdk_update_pre_set_output.go.tpl
@@ -1,8 +1,10 @@
 
 // sdk_update_pre_set_output hook
 //
-if delta.DifferentAt("Spec.Tags") {
-    if err = rm.syncTags(ctx, desired, latest); err != nil {
-        return nil, err
+if latest.ko.ObjectMeta.GetDeletionTimestamp() == nil {
+    if delta.DifferentAt("Spec.Tags") {
+        if err = rm.syncTags(ctx, desired, latest); err != nil {
+            return nil, err
+        }
     }
 }
